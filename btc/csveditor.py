3b4393618f88ed7b1f79d510e40238770fcc4b62
Joe Caparelli <joecaparelli@dhcp-rhodes-1073.eduroam.cornell.edu>
2018-03-05 23:03:05 -0500

    finished

--- btc/csveditor.py
+++
@@ -1,8 +0,0 @@
-import csv
-hs = open("at.txt","a")
-with open('crypto-markets.csv') as csvfile:
-	reader = csv.DictReader(csvfile)
-	for row in reader:
-		if row['symbol']=="BTC":
-			hs.write(row['date']+","+row['close']+'\n')
-hs.close()
